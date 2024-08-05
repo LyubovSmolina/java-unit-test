ad9ebe414286897121308452e7a85ac6804c4f11
Lyubov Smolina <turkhanova1994@yandex.ru>
2024-08-05 11:38:41 +0300

    My unit tests

---
+++ task_2.java
@@ -0,0 +1,39 @@
+  GNU nano 8.0                                            task_2.java
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+    assertEquals("Должно вернуться true, так как пользователь совершеннолетний", true, isAdult);
+}
+
+@Test
+    public void checkIsAdultWhenAgeIsLessThan18False() {
+        Program program1 = new Program();
+        boolean isAdult = program1.checkIsAdult(15);
+        assertEquals("Должно вернуться false, так как пользователь несовершеннолетний", false, isAdult);
+    }
+@Test
+    public void checkIsAdultWhenAgeIs18True() {
+    Program program18 = new Program ();
+    boolean isAdult = program18.checkIsAdult(18);
+    assertEquals("Должно вернуться true, так как пользователь совершеннолетний", true, isAdult);
+    }
